3f3c833e9c9716205c790c5818758aa76c369dd1
m3llington <mellingtonucf@knights.ucf.edu>
2019-08-27 22:39:49 -0400

    console logged response from mySQL, added filter to the prompts in order to resolve issue with receiving actual numbers

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,28 +26,39 @@ connection.connect(function(err) {
 
 // function which prompts the user for what action they should take
 function start() {
+
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // console.log((res));
+    for (var i = 0; i < res.length; i++){
+      console.log("\nID: " + res[i].id + "\nProduct Name: " + res[i].product_name + "\nDepartment: " + res[i].department_name + "\nPrice: " + res[i].price + "\nQuantity in Stock: " + res[i].stock_quantity + "\n----------------");
+    
+ }
+  
   inquirer
     .prompt([
     {
-      name: "Name",
+      name: "ID",
       type: "input",
       message: "What is the ID of the item you would like to buy?",
+      filter: Number
     },
     {
       name: "Quantity",
       type: "input",
-      message: "What amount of that item would you like?"  
+      message: "What amount of that item would you like?",
+      filter: Number 
     }
 ])
     .then(function(answer) {
       //answers from the ID prompt
-      if (answer.ID === "apple") {
+      if (answer.ID === 1) {
         console.log("ID test is working");
         connection.end();
       }
       //answers from the quantity prompt
-      else if (answer.Quantity === 1){
-        console.log("quantity test is working");
+      else if (answer.Quantity === 2){
+        console.log("Quantity test is working");
         connection.end();
       }
       else{
@@ -55,6 +66,8 @@ function start() {
         connection.end();
       }
     });
+});
+
 }
 
 //for loop console log in order to show the items that the user can choose
